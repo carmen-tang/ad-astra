84641947f1962d595a22fd57f29788c305b323b9
Carmen <carmen.tang1@gmail.com>
2023-12-18 19:04:56 -0800

    added chart

---
+++ src/app/components/ChartVisualization.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS } from 'chart.js/auto';
+
+export default function ChartVisualization() {
+	return (
+		<div>
+      <Bar
+        data={{
+          labels: ["January", "February", "March", "April", "May"],
+          datasets: [
+            {
+              label: "Number of Genomes Sequenced",
+              data: [150, 88, 60, 270, 42],
+              backgroundColor: ["#faf78e", "#fba7b6", "#b4d8c4", "#e1c5dd", "#6cb8f4"],
+            },
+          ]
+        }}
+        height={300}
+        width={500}
+        options={{
+          maintainAspectRatio: true
+        }}
+      />
+    </div>
+	);
+}
--- src/app/components/Pinned.js
+++ src/app/components/Pinned.js
@@ -12,6 +12,7 @@ import BacteriaIcon from './../assets/BacteriaIcon'
 import MicroscopeIcon from './../assets/MicroscopeIcon'
 import DNAIcon from './../assets/DNAIcon'
 import Journals from './Journals'
+import ChartVisualization from './ChartVisualization'
 
 export default function Pinned() {
 	return (
@@ -23,47 +24,13 @@ export default function Pinned() {
 			<Journals />
 			<div>
 				<div className="flex items-center justify-between">
-					<h2 className="font-regular text-stone-800 text-xl">My learnings</h2>
-					<a href="#more" title="More options">
+					<h2 className="font-regular text-stone-800 text-xl">Sequencing Progress</h2>
+					{/* <a href="#more" title="More options">
 						<MoreIcon />
-					</a>
+					</a> */}
 				</div>
-				<p className="my-2">Your progress of medical lectures</p>
 			</div>
-			<ul className="grid sm:grid-cols-2 gap-x-4 gap-y-8">
-				<li>
-					<ProgressCard
-						title={"Viruses"}
-						progress={"96%"}
-						icon={<VirusIcon />}
-						color={1}
-					/>
-				</li>
-				<li>
-					<ProgressCard
-						title={"Bacterial cells"}
-						progress={"33%"}
-						icon={<BacteriaIcon />}
-						color={2}
-					/>
-				</li>
-				<li>
-					<ProgressCard
-						title={"Med science"}
-						progress={"7%"}
-						icon={<MicroscopeIcon />}
-						color={3}
-					/>
-				</li>
-				<li>
-					<ProgressCard
-						title={"Human genes"}
-						progress={"58%"}
-						icon={<DNAIcon />}
-						color={4}
-					/>
-				</li>
-			</ul>
+			<ChartVisualization />
 		</section>
 	);
 }
